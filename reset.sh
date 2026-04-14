7c3580533375c709c10abfba641b4729ae4c5d9f
David Straub <straub@protonmail.com>
2026-04-14 09:04:50 +0200

    Cleanup to prevent disk space issues

--- reset.sh
+++ reset.sh
@@ -6,23 +6,24 @@ cd /opt/grampsweb
 docker-compose down
 
 # delete all data
-docker-compose run --entrypoint="" grampsweb bash -c 'rm -rf /app/indexdir/* && rm -rf /app/thumbnail_cache/* && rm -rf /app/users/* && rm -rf /app/media/*'
+docker-compose run --rm --entrypoint="" grampsweb bash -c 'rm -rf /app/indexdir/* && rm -rf /app/thumbnail_cache/* && rm -rf /app/users/* && rm -rf /app/media/*'
 
 # create new secret token (requires users to log in again)
-docker-compose run --entrypoint="" grampsweb bash -c 'python3 -c "import secrets;print(secrets.token_urlsafe(32))"  | tr -d "\n" > /app/secret/secret'
+docker-compose run --rm --entrypoint="" grampsweb bash -c 'python3 -c "import secrets;print(secrets.token_urlsafe(32))"  | tr -d "\n" > /app/secret/secret'
 
 # copy media files of example.gramps
-docker-compose run --entrypoint="" grampsweb cp -a /usr/local/share/doc/gramps/example/gramps/. /app/media/
+docker-compose run --rm --entrypoint="" grampsweb cp -a /usr/local/share/doc/gramps/example/gramps/. /app/media/
 
 # import Gramps example database
-docker-compose run --entrypoint="" grampsweb bash -c 'cp -r /usr/local/share/doc/gramps/example/gramps/example.gramps /app && rm -rf /root/.gramps/grampsdb/* && gramps -C Gramps\ Web -i example.gramps --config=database.backend:sqlite --config=database.path:/root/.gramps/grampsdb'
+docker-compose run --rm --entrypoint="" grampsweb bash -c 'cp -r /usr/local/share/doc/gramps/example/gramps/example.gramps /app && rm -rf /root/.gramps/grampsdb/* && gramps -C Gramps\ Web -i example.gramps --config=database.backend:sqlite --config=database.path:/root/.gramps/grampsdb'
 
 # recreate search index
-docker-compose run grampsweb python3 -m gramps_webapi --config /app/config/config.cfg search index-full
+docker-compose run --rm grampsweb python3 -m gramps_webapi --config /app/config/config.cfg search index-full
 
 # create user accounts
-docker-compose run grampsweb bash -c 'python3 -m gramps_webapi --config /app/config/config.cfg user add owner owner --fullname Owner --role 4 && python3 -m gramps_webapi  --config /app/config/config.cfg user add editor editor --fullname Editor --role 3 && python3 -m gramps_webapi  --config /app/config/config.cfg user add contributor contributor --fullname Contributor --role 2 && python3 -m gramps_webapi  --config /app/config/config.cfg user add member member --fullname Member --role 1'
+docker-compose run --rm grampsweb bash -c 'python3 -m gramps_webapi --config /app/config/config.cfg user add owner owner --fullname Owner --role 4 && python3 -m gramps_webapi  --config /app/config/config.cfg user add editor editor --fullname Editor --role 3 && python3 -m gramps_webapi  --config /app/config/config.cfg user add contributor contributor --fullname Contributor --role 2 && python3 -m gramps_webapi  --config /app/config/config.cfg user add member member --fullname Member --role 1'
 
-# update image
+# update image and remove old/unused images and stopped containers
 docker-compose pull grampsweb
+docker system prune -f
 docker-compose up -d
